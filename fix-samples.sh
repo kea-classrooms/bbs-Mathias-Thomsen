38ffc850c13a548864de26fe94709a86c197e293
MT <113047767+Mathias-Thomsen@users.noreply.github.com>
2023-02-06 13:21:41 +0000

    Im done

--- fix-samples.sh
+++ fix-samples.sh
@@ -1,7 +1,8 @@
-cd samples/animals 
-mv countries ..
-
-cd .. 
-
-cd countries/
-mv movies ..
+cd $CODESPACE_VSCODE_FOLDER/samples/
+mv animals/countries/ .
+mv countries/movies/ . 
+mv animals/Monkies.txt animals/Monkeys.txt
+echo Monkeys > animals/Monkeys.txt
+mv movies/Germany.txt countries/
+mv countries/.apes.txt animals/
+cd - 
